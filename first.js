0c7229812041c571ab696a4fafe5044acf8fe416
Chirag <chiragsaini157@gmail.com>
2025-03-29 18:04:13 +0530

    second commit

--- first.js
+++ first.js
@@ -1,3 +1,21 @@
 function addNumbers(a, b) {
     return a + b;
 }
+
+
+const account = {
+    name: 'John Doe',
+    balance: 1000,
+    deposit(amount) {
+        this.balance += amount;
+        console.log(`Deposited ${amount}. New balance is ${this.balance}`);
+    },
+    withdraw(amount) {
+        if (amount > this.balance) {
+            console.log('Insufficient funds');
+        } else {
+            this.balance -= amount;
+            console.log(`Withdrew ${amount}. New balance is ${this.balance}`);
+        }
+    }
+};
